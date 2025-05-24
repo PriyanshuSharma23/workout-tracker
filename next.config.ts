35cc95f54de01d7792f5f3724a8733144bc91fe5
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2025-05-24 18:58:34 +0530

    :bug: Home page stuck on 12 May

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,17 @@ const nextConfig: NextConfig = {
   /* config options here */
   async headers() {
     return [
-      // {
-      //   source: "/((?!_next/static|_next/image|favicon.ico).*)", // Match all routes
-      //   headers: [
-      //     {
-      //       key: "Cache-Control",
-      //       value: "no-store, no-cache, must-revalidate, proxy-revalidate",
-      //     },
-      //     { key: "Pragma", value: "no-cache" },
-      //     { key: "Expires", value: "0" },
-      //   ],
-      // },
+      {
+        source: "/((?!_next/static|_next/image|favicon.ico).*)", // Match all routes
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+          { key: "Pragma", value: "no-cache" },
+          { key: "Expires", value: "0" },
+        ],
+      },
     ];
   },
 };
